a97c76682a652c560b2b79dac5fc304f206749f3
Nishanth S <nishanth.s@pal-erp.com>
2025-06-25 14:56:18 +0530

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from '@svgr/rollup';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const base = process.env.VITE_ENV === 'prod'
+    ? '/resume-ranker/'
+    : '/';
+  return defineConfig({
+    base,
+    plugins: [react(), svgr()],
+    server: {
+      watch: { usePolling: true },
+      port: 3000,
+      open: base
+    },
+  })
+}
