33a3197a4112dc38218a3c74fcecfead1cf0cc3c
Nishanth S <nishanth.s@pal-erp.com>
2025-08-12 10:57:53 +0530

    issue fixing

--- src/components/ResumeCard/index.jsx
+++ src/components/ResumeCard/index.jsx
@@ -82,7 +82,7 @@ const ResumeCard = ({ resume }) => {
                     {jobTitle && <p>{jobTitle}</p>}
                     {email && <p>{email}</p>}
                 </div>
-                <CircularProgress value={weightedScore} />
+                {weightedScore && <CircularProgress value={weightedScore} />}
             </div>
             {
                 section_scores && Object.keys(section_scores).length > 0 && (
